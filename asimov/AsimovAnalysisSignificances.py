e55e4842536ecf36a5e9a5ab156a8edd31f5c51b
steven-j-wren <steven.wren@icecube.wisc.edu>
2016-03-09 15:29:41 +0000

    Adding ability to output plots of best fit points

--- asimov/AsimovAnalysisSignificances.py
+++ asimov/AsimovAnalysisSignificances.py
@@ -24,6 +24,10 @@ parser.add_argument('-th23','--theta23', action='store_true', default=False,
                     help="Analysing as a function of theta23")
 parser.add_argument('-l','--livetime', action='store_true', default=False,
                     help="Analysis as a function of livetime")
+parser.add_argument('-IB','--individual_bests',action='store_true',default=False,
+                    help="Flag to plot best fit points individually")
+parser.add_argument('-CB','--combined_bests',action='store_true',default=False,
+                    help="Flag to plot best fit points combined")
 parser.add_argument('-v', '--verbose', action='count', default=None,
                     help='set verbosity level')
 args = parser.parse_args()
@@ -34,6 +38,27 @@ true_h_fid_dir = args.true_h_fid_dir
 false_h_best_fit_dir = args.false_h_best_fit_dir
 theta23analysis = args.theta23
 livetimeanalysis = args.livetime
+IBests = args.individual_bests
+CBests = args.combined_bests
+
+# Make a nice dict to contain LaTeX labels for everything
+axislabels = {}
+axislabels['nu_nubar_ratio'] = r"$\nu/\bar{\nu}$ Ratio"
+axislabels['nue_numu_ratio'] = r"$\nu_e/\nu_{\mu}$ Ratio"
+axislabels['aeff_scale'] = r"$A_{eff}$ Scale"
+axislabels['theta23_nh'] = r"$\theta_{23}$"
+axislabels['theta23_ih'] = r"$\theta_{23}$"
+axislabels['theta23'] = r"$\theta_{23}$"
+axislabels['theta13'] = r"$\theta_{13}$"
+axislabels['energy_scale'] = r"Energy Scale"
+axislabels['atm_delta_index'] = r"Atmospheric Index"
+axislabels['deltam31_nh'] = r"$\Delta m^2_{31}$"
+axislabels['deltam31_ih'] = r"$\Delta m^2_{31}$"
+axislabels['deltam31'] = r"$\Delta m^2_{31}$"
+axislabels['icc_atmos_mu_scale'] = r"Muon Background Scale"
+axislabels['muon_background_scale'] = r"Muon Background Scale"
+axislabels['llh'] = r"Log Likelihood"
+axislabels['chisquare'] = r'$\chi^2$'
 
 theta23vals = []
 sin2theta23vals = []
@@ -236,6 +261,354 @@ if theta23analysis == True:
     plt.axvline(NFSOSin2Theta23, linestyle='--', color='b', label='IO Best Fit')
     plt.legend(['Normal','Inverted','NO Best Fit','IO Best Fit'],loc='upper left')
     plt.savefig('%s_%s_Sin2Theta23SignificanceswBestFits.png'%(detector,selection))
+    plt.close()
+
+############################################################
+
+############ Get Best Fit Points out of Files
+
+############################################################
+
+if IBests == True or CBests == True:
+
+    params = {}
+    params['data_NMH'] = {}
+    params['data_IMH'] = {}
+    params['data_NMH']['true_h_fiducial'] = {}
+    params['data_NMH']['false_h_best'] = {}
+    params['data_IMH']['true_h_fiducial'] = {}
+    params['data_IMH']['false_h_best'] = {}
+
+    for infile in sorted(os.listdir(true_h_fid_dir)):
+        if os.path.isfile(true_h_fid_dir+infile):
+            indict = from_json(true_h_fid_dir+infile)
+            try:
+                fparams = indict['results']['data_NMH']['hypo_IMH'][0]
+            except:
+                fparams = indict['results']['data_NMH']['hypo_IMH']
+            for pkey in fparams.keys():
+                params['data_NMH']['true_h_fiducial'][pkey] = []
+                params['data_NMH']['false_h_best'][pkey] = []
+                params['data_IMH']['true_h_fiducial'][pkey] = []
+                params['data_IMH']['false_h_best'][pkey] = []
+
+    for theta23 in theta23vals:
+    
+        # Get chisquare values for true_h_fiducial distributions
+        for trueinfile in sorted(os.listdir(true_h_fid_dir)):
+            if os.path.isfile(true_h_fid_dir+trueinfile):
+                indict = from_json(true_h_fid_dir+trueinfile)
+                theta23_nh = indict['template_settings']['params']['theta23_nh']['value']
+                theta23_ih = indict['template_settings']['params']['theta23_ih']['value']
+                assert(theta23_nh == theta23_ih)
+                if theta23_nh == theta23:
+                    for data_tag in indict['results'].keys():
+                        if 'NMH' in data_tag or 'NH' in data_tag:
+                            hypo_tag = 'hypo_IMH'
+                        if 'IMH' in data_tag or 'IH' in data_tag:
+                            hypo_tag = 'hypo_NMH'
+                        try:
+                            res = indict['results'][data_tag][hypo_tag][0]
+                        except:
+                            res = indict['results'][data_tag][hypo_tag]
+                        for pkey in res.keys():
+                            param = res[pkey][0]
+                            if pkey == 'theta13':
+                                params[data_tag]['true_h_fiducial'][pkey].append(180.0*param/math.pi)
+                            elif pkey == 'theta23':
+                                params[data_tag]['true_h_fiducial'][pkey].append(math.pow(math.sin(param),2))
+                            elif pkey == 'deltam31':
+                                params[data_tag]['true_h_fiducial'][pkey].append(math.fabs(param))
+                            else:
+                                params[data_tag]['true_h_fiducial'][pkey].append(param)
+
+        # Get chisquare values for false_h_best_fit distributions
+        for falseinfile in sorted(os.listdir(false_h_best_fit_dir)):
+            if os.path.isfile(false_h_best_fit_dir+falseinfile):
+                sin2theta23 = math.pow(math.sin(theta23),2)
+                splits1 = falseinfile.lower().split('sin2theta23')
+                splits2 = splits1[-1].split('data')
+                try:
+                    float(splits2[0])
+                except:
+                    splits2 = splits2[0].split('_')
+                try:
+                    RightFile = "%.4f"%sin2theta23 == splits2[0]
+                except:
+                    RightFile = "%.2f"%sin2theta23 == splits2[0]
+                if RightFile == True:
+                    indict = from_json(false_h_best_fit_dir+falseinfile)
+                    for data_tag in indict['results'].keys():
+                        if 'NMH' in data_tag or 'NH' in data_tag:
+                            hypo_tag = 'hypo_IMH'
+                        if 'IMH' in data_tag or 'IH' in data_tag:
+                            hypo_tag = 'hypo_NMH'
+                        try:
+                            res = indict['results'][data_tag][hypo_tag][0]
+                        except:
+                            res = indict['results'][data_tag][hypo_tag]
+                        for pkey in res.keys():
+                            param = res[pkey][0]
+                            if pkey == 'theta13':
+                                params[data_tag]['false_h_best'][pkey].append(180.0*param/math.pi)
+                            elif pkey == 'theta23':
+                                params[data_tag]['false_h_best'][pkey].append(math.pow(math.sin(param),2))
+                            elif pkey == 'deltam31':
+                                params[data_tag]['false_h_best'][pkey].append(math.fabs(param))
+                            else:
+                                params[data_tag]['false_h_best'][pkey].append(param)
+
+    for livetime in livetimevals:
+
+        # Get chisquare values for true_h_fiducial distributions
+        for trueinfile in sorted(os.listdir(true_h_fid_dir)):
+            if os.path.isfile(true_h_fid_dir+trueinfile):
+                indict = from_json(true_h_fid_dir+trueinfile)
+                livetime_val = indict['template_settings']['params']['livetime']['value']
+                if livetime_val == livetime:
+                    for data_tag in indict['results'].keys():
+                        if 'NMH' in data_tag or 'NH' in data_tag:
+                            hypo_tag = 'hypo_IMH'
+                        if 'IMH' in data_tag or 'IH' in data_tag:
+                            hypo_tag = 'hypo_NMH'
+                        try:
+                            res = indict['results'][data_tag][hypo_tag][0]
+                        except:
+                            res = indict['results'][data_tag][hypo_tag]
+                        for pkey in res.keys():
+                            param = res[pkey][0]
+                            if pkey == 'theta13':
+                                params[data_tag]['true_h_fiducial'][pkey].append(180.0*param/math.pi)
+                            elif pkey == 'theta23':
+                                params[data_tag]['true_h_fiducial'][pkey].append(math.pow(math.sin(param),2))
+                            elif pkey == 'deltam31':
+                                params[data_tag]['true_h_fiducial'][pkey].append(math.fabs(param))
+                            else:
+                                params[data_tag]['true_h_fiducial'][pkey].append(param)
+                            
+        # Get chisquare values for false_h_best_fit distributions
+        for falseinfile in sorted(os.listdir(false_h_best_fit_dir)):
+            if os.path.isfile(false_h_best_fit_dir+falseinfile):
+                splits1 = falseinfile.split('livetime')
+                splits2 = splits1[-1].split('Data')
+                if "%.2f"%livetime == splits2[0]:
+                    indict = from_json(false_h_best_fit_dir+falseinfile)
+                    for data_tag in indict['results'].keys():
+                        if 'NMH' in data_tag or 'NH' in data_tag:
+                            hypo_tag = 'hypo_IMH'
+                        if 'IMH' in data_tag or 'IH' in data_tag:
+                            hypo_tag = 'hypo_NMH'
+                        try:
+                            res = indict['results'][data_tag][hypo_tag][0]
+                        except:
+                            res = indict['results'][data_tag][hypo_tag]
+                        for pkey in res.keys():
+                            param = res[pkey][0]
+                            if pkey == 'theta13':
+                                params[data_tag]['false_h_best'][pkey].append(180.0*param/math.pi)
+                            elif pkey == 'theta23':
+                                params[data_tag]['false_h_best'][pkey].append(math.pow(math.sin(param),2))
+                            elif pkey == 'deltam31':
+                                params[data_tag]['false_h_best'][pkey].append(math.fabs(param))
+                            else:
+                                params[data_tag]['false_h_best'][pkey].append(param)
+
+############################################################
+
+############ Plot Individual Best Fit Plots
+
+############################################################
+
+if IBests:
+
+    if theta23analysis == True:
+        x = np.array(sin2theta23vals)
+        xlabel = r"$\sin^2\theta_{23}$"
+        xmin = 0.30
+        xmax = 0.70
+
+    if livetimeanalysis == True:
+        x = np.array(livetimevals)
+        xlabel = "Livetime [yrs]"
+        xmin = 2.
+        xmax = 11.
+
+    for pkey in params['data_NMH']['true_h_fiducial'].keys():
+        for fkey in params['data_NMH'].keys():
+
+            if fkey == 'true_h_fiducial':
+                ftitle = 'True Hierarchy Fiducial'
+            if fkey == 'false_h_best':
+                ftitle = 'False Hierarchy Best Fit'    
+
+            if theta23analysis == True:
+                title = "%s %s Event Selection Best Fit Values for 3 years Livetime"%(detector,selection)
+                #title = "%s %s Event Selection Best Fit Values for 10 years Livetime"%(detector,selection)
+                filename = '%s_%s_Sin2Theta23BestFitValues.png'%(detector,selection)
+
+            if livetimeanalysis == True:
+                title = r"%s %s Event Selection Best Fit Values for Nu-Fit 2014 $\theta_{23}$ values"%(detector,selection)
+                #title = r"%s %s Event Selection Best Fit Values for $\theta_{23}=42.3^{\circ}$"%(detector,selection)
+                #title = r"%s %s Event Selection Best Fit Values for $\theta_{23}=49.5^{\circ}$"%(detector,selection)
+                filename = '%s_%s_LivetimeBestFitValues.png'%(detector,selection)
+
+            ylabel = axislabels[pkey]
+        
+            yTHF = np.array(params['data_NMH'][fkey][pkey])
+            yFHB = np.array(params['data_IMH'][fkey][pkey])
+
+            dyTHF = yTHF.max() - yTHF.min()
+            dyFHB = yFHB.max() - yFHB.min()
+
+            yminTHF = yTHF.min() - dyTHF*0.10
+            ymaxTHF = yTHF.max() + dyTHF*0.10
+            yminFHB = yFHB.min() - dyTHF*0.10
+            ymaxFHB = yFHB.max() + dyTHF*0.10
+
+            if yminTHF < yminFHB:
+                ymin = yminTHF
+            else:
+                ymin = yminFHB
+
+            if ymaxTHF > ymaxFHB:
+                ymax = ymaxTHF
+            else:
+                ymax = ymaxFHB
+        
+            plt.plot(x,yTHF,color='r',marker="o")
+            plt.plot(x,yFHB,color='b',marker="o")
+            plt.axis([xmin, xmax, ymin, ymax])
+            plt.legend(['True NO','True IO'],loc='best')
+            plt.xlabel(xlabel)
+            plt.ylabel(ylabel)
+            title = title + r"\\" + ylabel + " (" + ftitle + ")"
+            filename = fkey + "_" + pkey + "_" + filename
+            plt.title(title, ha='center')
+            plt.savefig(filename)
+            plt.close()
+
+if CBests:
+
+    if theta23analysis == True:
+        MainTitle = "%s %s Event Selection Best Fit Values for 3 years Livetime"%(detector,selection)
+        #MainTitle = "%s %s Event Selection Best Fit Values for 10 years Livetime"%(detector,selection)
+
+        x = np.array(sin2theta23vals)
+        xlabel = r"$\sin^2\theta_{23}$"
+        xmin = 0.30
+        xmax = 0.70
+
+        basefilename = '%s_%s_Sin2Theta23BestFitValues.png'%(detector,selection)
+
+    if livetimeanalysis == True:
+        MainTitle = r"%s %s Event Selection Best Fit Values for Nu-Fit 2014 $\theta_{23}$ values"%(detector,selection)
+        #MainTitle = r"%s %s Event Selection Best Fit Values for $\theta_{23}=42.3^{\circ}$"%(detector,selection)
+        #MainTitle = r"%s %s Event Selection Best Fit Values for $\theta_{23}=49.5^{\circ}$"%(detector,selection)
+
+        basefilename = '%s_%s_LivetimeBestFitValues.png'%(detector,selection)
+
+        x = np.array(livetimevals)
+        xlabel = "Livetime [yrs]"
+        xmin = 2.
+        xmax = 11.
+
+    for pkey in params['data_NMH']['true_h_fiducial'].keys():
+        plt.figure(figsize=(8,4))
+        subplot_num = 1
+        for fkey in params['data_NMH'].keys():
+
+            if fkey == 'true_h_fiducial':
+                ftitle = 'True Hierarchy Fiducial'
+            if fkey == 'false_h_best':
+                ftitle = 'False Hierarchy Best Fit'    
+
+            ylabel = axislabels[pkey]
+        
+            yTHF = np.array(params['data_NMH'][fkey][pkey])
+            yFHB = np.array(params['data_IMH'][fkey][pkey])
+
+            dyTHF = yTHF.max() - yTHF.min()
+            dyFHB = yFHB.max() - yFHB.min()
+
+            yminTHF = yTHF.min() - dyTHF*0.10
+            ymaxTHF = yTHF.max() + dyTHF*0.10
+            yminFHB = yFHB.min() - dyTHF*0.10
+            ymaxFHB = yFHB.max() + dyTHF*0.10
+
+            if yminTHF < yminFHB:
+                ymin = yminTHF
+            else:
+                ymin = yminFHB
+
+            if ymaxTHF > ymaxFHB:
+                ymax = ymaxTHF
+            else:
+                ymax = ymaxFHB
+
+            plt.subplot(1,2,subplot_num)
+            plt.plot(x,yTHF,color='r',marker="o")
+            plt.plot(x,yFHB,color='b',marker="o")
+            plt.axis([xmin, xmax, ymin, ymax])
+            plt.legend(['True NO','True IO'],loc='best')
+            plt.xlabel(xlabel)
+            plt.ylabel(ylabel)
+            plt.title(ftitle, ha='center')
+            subplot_num +=1
+        plt.suptitle(MainTitle + " (%s)"%ylabel, fontsize=14)
+        plt.tight_layout()
+        plt.subplots_adjust(top=0.85)
+        filename = "both_fits_" + pkey + "_" + basefilename
+        plt.savefig(filename)
+        plt.close()
+
+    for fkey in params['data_NMH'].keys():
+        plt.figure(figsize=(20,8))
+        subplot_num = 1
+        for pkey in params['data_NMH']['true_h_fiducial'].keys():
+
+            if fkey == 'true_h_fiducial':
+                ftitle = 'True Hierarchy Fiducial'
+            if fkey == 'false_h_best':
+                ftitle = 'False Hierarchy Best Fit'    
+
+            ylabel = axislabels[pkey]
+        
+            yTHF = np.array(params['data_NMH'][fkey][pkey])
+            yFHB = np.array(params['data_IMH'][fkey][pkey])
+
+            dyTHF = yTHF.max() - yTHF.min()
+            dyFHB = yFHB.max() - yFHB.min()
+
+            yminTHF = yTHF.min() - dyTHF*0.10
+            ymaxTHF = yTHF.max() + dyTHF*0.10
+            yminFHB = yFHB.min() - dyTHF*0.10
+            ymaxFHB = yFHB.max() + dyTHF*0.10
+
+            if yminTHF < yminFHB:
+                ymin = yminTHF
+            else:
+                ymin = yminFHB
+
+            if ymaxTHF > ymaxFHB:
+                ymax = ymaxTHF
+            else:
+                ymax = ymaxFHB
+
+            plt.subplot(2,5,subplot_num)
+            plt.plot(x,yTHF,color='r',marker="o")
+            plt.plot(x,yFHB,color='b',marker="o")
+            plt.axis([xmin, xmax, ymin, ymax])
+            plt.legend(['True NO','True IO'],loc='best')
+            plt.xlabel(xlabel)
+            plt.ylabel(ylabel)
+            plt.title(ftitle, ha='center')
+            subplot_num +=1
+        plt.suptitle(MainTitle + " " + ftitle, fontsize=36)
+        plt.tight_layout()
+        plt.subplots_adjust(top=0.85)
+        filename = fkey + "_all_systs_" + basefilename
+        plt.savefig(filename)
+        plt.close()
 
 
                         
