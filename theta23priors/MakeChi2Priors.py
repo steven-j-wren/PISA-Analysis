8964e4592b25977666cd05fd09aec4ca7113f7d5
steven-j-wren <steven.wren@icecube.wisc.edu>
2016-05-19 13:54:25 +0100

    Making the output nicer to copy in to PISA files

--- theta23priors/MakeChi2Priors.py
+++ theta23priors/MakeChi2Priors.py
@@ -14,13 +14,19 @@ x, y = np.loadtxt(args.chi2_file,unpack=True)
 
 f = scipy.interpolate.splrep(x,y,s=0)
 
+wholedict = {}
+wholedict["params"] = {}
+wholedict["params"]["param"] = {}
+
 prior = {}
 prior["coeffs"] = f[1]
 prior["deg"] = f[2]
 prior["knots"] = f[0]
 prior["kind"] = "spline"
 
-to_json(prior, args.chi2_file.split(".txt")[0]+"SplinePrior.json")
+wholedict["params"]["param"]["prior"] = prior
+
+to_json(wholedict, args.chi2_file.split(".txt")[0]+"SplinePrior.json")
 
 
 
